30991e4d9d837a7632881e42b290b4ca4a9b7aff
Harika-Cse <23jr1a0589@gmail.com>
2026-09-01 12:30:01 +0530

    Create 06_list_operations.py

---
+++ python_programs/06_list_operations.py
@@ -0,0 +1,10 @@
+numbers = [10, 20, 30, 40, 50]
+
+print("List:", numbers)
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+numbers.append(60)
+
+print("After adding 60:", numbers)
+print("Length of list:", len(numbers))
